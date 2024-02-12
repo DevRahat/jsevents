6cc0fba03da62c7eebc430f308d8d3815a6ad839
DevRahat <dev.rahatislam@gmail.com>
2024-02-12 14:23:11 +0600

    green

--- js/events.js
+++ js/events.js
@@ -19,3 +19,19 @@ const makePurple=document.getElementById('make-purple');
         makePurple.onclick=function purple(){
             document.body.style.backgroundColor='purple';
         }
+
+
+// Option 4 
+const pinkButton=document.getElementById('make-pink');
+        pinkButton.addEventListener('click',makPink);
+        function makPink(){
+            document.body.style.backgroundColor='pink';
+        }
+
+
+
+        const makeGreenButton=document.getElementById('make-green');
+        makeGreenButton.addEventListener('click', function makeGreen(){
+            document.body.style.backgroundColor='green';
+        })
+ 
